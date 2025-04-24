aff0a7b4c8231606c1f64fad3bef21b84e23cb13
Luciano Tres <lucianotres@outlook.com>
2025-04-24 18:06:06 -0300

    First project detailed with screenshot

--- public/data.ts
+++ public/data.ts
@@ -1,23 +1,22 @@
 export const dataAbout = [
   {
-    title: "Projeto 1",
+    title: "ERP para provedor de internet",
     paragraph:
-      "Desenvolvimento ....",
-    arraySkills: ["Bootstrap 4 CSS", "Javascript", "jQuery", "ASP.Net", "Microsserviços"],
-    image: "/images/tela1.png",
+      "Manutenção e atualizações em sistema legado de empresa de internet. O software foi desenvolvido internamente pela equipe da empresa e conforme o seu crescimento necessitou de melhorias e novas funcionalidades. Trata-se de um software ERP para cadastros, controles administrativos, ordens de serviço, estoque, financeiro e faturamento. Abrange todas as áreas da empresa e voltado para seu uso exclusivo.",
+    arraySkills: ["Delphi", "XML", "PostgreSQL", "Radius", "Mikrotik API"],
+    image: "/images/erp1.png",
     link: "",
     sizeImg: {
-      width: 650,
-      height: 489,
+      width: 800,
+      height: 616,
     },
-    aspectRatio: "4/3",
     moreDetails: {
-      image: "/images/tela2.png",
-      titles: ["Topico 1", "Topico 2", "Topico 2"],
+      image: "/images/erp2.png",
+      titles: ["Manutenção", "Atualizações", "Benefícios adquiridos"],
       texts: [
-        "Texto complexo aqui",
-        "Linha 2",
-        "Linha 3",
+        "Garantir o funcionamento do sistema, correção de problemas conhecidos, gerenciamento de backups, atualizações fiscais e integrações contábeis. Apesar de ser um software legado desenvolvido em Delphi, estava diretamente integrado com a operação da empresa, sendo de extremo valor para ela.",
+        "Na medida que o negócio foi evoluindo, a empresa necessitou de novos cadastros, melhorias nos atuais e diversas ferramentas gerenciais para análise de resultados. A forma mais rápida para o momento foi dar continuidade ao desenvolvimento na mesma plataforma.",
+        "Tanto a empresa se beneficiou com meu trabalho prestado sob este sistema quanto eu mesmo. Este foi um período de muito aprendizado em relação ao mundo de redes de computadores e como isto se aplica para o funcionamento da internet. Bem como solidou muito meu conhecimento na linguagem Delphi e em banco de dados relacional PostgreSQL. ",
       ],
     },
   }
